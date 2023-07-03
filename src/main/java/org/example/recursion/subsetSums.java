493155930825769ee132522402956161982bdbd5
Shivakant6387 <shivakant5875@gmail.com>
2023-07-03 23:46:19 +0530

    oops

--- src/main/java/org/example/recursion/subsetSums.java
+++ src/main/java/org/example/recursion/subsetSums.java
@@ -1,7 +1,20 @@
 package org.example.recursion;
 
 public class subsetSums {
+    static boolean isThereSubsetSum(int arr[],int n,int sum){
+        if (sum==0)
+            return true;
+        if (n==0)
+            return false;
+        if (arr[n-1]>sum)
+            return isThereSubsetSum(arr,n-1,sum);
+        return isThereSubsetSum(arr,n-1,sum)||isThereSubsetSum(arr,n-1,sum-arr[n-1]);
+    }
     public static void main(String[] args) {
+        int []arr={1,2,3};
+        int n=arr.length;
+        int sum=0;
+        System.out.println(isThereSubsetSum(arr,n,sum));
 
     }
 }
